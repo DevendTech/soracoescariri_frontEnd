1b3a3a19711228e1f4ea5bb42bfeeab9e3dbc8e4
MatAndrade01 <ltbmatheus141012@gmail.com>
2025-04-23 00:49:55 -0300

    Tela de Login com POP-UPS ok!

--- assets/scripts/telaLogin.js
+++ assets/scripts/telaLogin.js
@@ -8,8 +8,38 @@ form.addEventListener('submit', async (event) => {
   const email = inputLogin.value.trim();
   const password = inputPassword.value.trim();
 
+  const divMensage = document.getElementById('mensage');
+
   if (!email || !password) {
-    alert('Por favor, preencha todos os campos.');
+    divMensage.classList.remove('hidden');
+    divMensage.innerHTML = `
+      <div
+        role="alert"
+        class="bg-yellow-100 dark:bg-yellow-900 border-l-4 border-yellow-500 dark:border-yellow-700 text-yellow-900 dark:text-yellow-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-yellow-200 dark:hover:bg-yellow-800 transform hover:scale-105"
+      >
+        <svg
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          class="h-5 w-5 flex-shrink-0 mr-2 text-yellow-600"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            stroke-width="2"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+        <p class="text-xs font-semibold">
+          Alerta - Preencha os campos corretamente.
+        </p>
+      </div>
+    `
+    setTimeout(() => {
+      divMensage.innerHTML = '';
+      divMensage.classList.add('hidden');
+    }, 4000);
     return;
   }
 
@@ -26,7 +56,30 @@ form.addEventListener('submit', async (event) => {
 
     if (!response.ok) {
       const errorData = await response.json();
-      alert(errorData.message || 'Falha no login. Tente novamente.');
+      console.log('Erro:', errorData);
+      divMensage.classList.remove('hidden');
+      duvMensage.innerHTML = `
+        <div
+          role="alert"
+          class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+        >
+          <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+          <p class="text-xs font-semibold">Error - Falha ao fazer o login.</p>
+        </div>
+      `
       return;
     }
 
@@ -41,10 +94,62 @@ form.addEventListener('submit', async (event) => {
       // Redireciona apenas se o token foi salvo com sucesso
       window.location.href = 'http://127.0.0.1:5500/pages/propietario/telaPropietario.html';
     } else {
-      alert('Falha ao salvar o token. Tente novamente.');
+      divMensage.classList.remove('hidden');
+      divMensage.innerHTML = `
+        <div
+          role="alert"
+          class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+        >
+          <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+          <p class="text-xs font-semibold">Error - Falha ao salvar o token. Tente novamente.!</p>
+        </div>
+      `
+      setTimeout(() => {
+        divMensage.innerHTML = '';
+        divMensage.classList.add('hidden');
+      }, 6000);
     }
   } catch (error) {
     console.error('Erro ao conectar ao servidor:', error);
-    alert('Erro ao conectar ao servidor. Tente novamente mais tarde.');
+    divMensage.classList.remove('hidden');
+      divMensage.innerHTML = `
+        <div
+          role="alert"
+          class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+        >
+          <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+          <p class="text-xs font-semibold">Error - E-mail ou Senha incorreto(a)!</p>
+        </div>
+      `
+      setTimeout(() => {
+        divMensage.innerHTML = '';
+        divMensage.classList.add('hidden');
+      }, 6000);
   }
 });
