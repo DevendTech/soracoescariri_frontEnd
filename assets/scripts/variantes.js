bf5f1c1eca3822d51443341956896fd9eb19cfc4
MatAndrade01 <ltbmatheus141012@gmail.com>
2025-04-24 22:47:59 -0300

    Pop-ups da tela de criar variantes ok!

--- assets/scripts/variantes.js
+++ assets/scripts/variantes.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get("productId");
   const productname = urlParams.get("productName")
+  const checkedNao = document.getElementById('checkedNao')
+  const alertaSucesso = document.getElementById('alertaSucesso')
 
   container.innerHTML = "";
 
@@ -42,7 +44,35 @@ document.addEventListener("DOMContentLoaded", function () {
     const discount = document.querySelector('input[name="desconto-radio"]:checked').value === "SIM" ? document.getElementById('input-desconto').value : 0;
 
     if (!weight || !price) {
-      alert("Por favor, preencha todos os campos da variante.");
+      alertaSucesso.classList.remove('hidden');
+      alertaSucesso.innerHTML = `
+        <div
+        role="alert"
+        class="bg-yellow-100 dark:bg-yellow-900 border-l-4 border-yellow-500 dark:border-yellow-700 text-yellow-900 dark:text-yellow-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-yellow-200 dark:hover:bg-yellow-800 transform hover:scale-105"
+        >
+          <svg
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          class="h-5 w-5 flex-shrink-0 mr-2 text-yellow-600"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+            d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            stroke-width="2"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            ></path>
+          </svg>
+          <p class="text-xs font-semibold">
+          Alerta - Preencha os campos corretamente.
+          </p>
+        </div>
+      `
+      setTimeout(() => {
+        divMensage.innerHTML = '';
+        divMensage.classList.add('hidden');
+      }, 4000);
       return;
     }
 
@@ -62,13 +92,66 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then(response => response.json())
       .then(data => {
-        console.log("Variante adicionada:", data);
-        alert("Variante adicionada com sucesso!");
+        alertaSucesso.classList.remove("hidden");
+        alertaSucesso.classList.add("flex");
+        alertaSucesso.innerHTML = `
+          <div
+          role="alert"
+          class="bg-green-100 dark:bg-green-900 border-l-4 border-green-500 dark:border-green-700 text-green-900 dark:text-green-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-green-200 dark:hover:bg-green-800 transform hover:scale-105"
+          >
+            <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 flex-shrink-0 mr-2 text-green-600"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+              d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              ></path>
+            </svg>
+            <p class="text-xs font-semibold">Sucesso - Variante criada com sucesso!</p>
+          </div>
+        `
+        setTimeout(() => {
+          document.getElementById("alertaSucesso").classList.add("hidden");
+          checkedNao.checked = true;
+        }, 4000);
+        inputDesconto.disabled = true;
+        inputDesconto.classList.add('bg-gray-400');
         formAdicionarVariantes.reset();
       })
       .catch(error => {
-        console.error("Erro ao adicionar variante:", error);
-        alert("Erro ao adicionar variante. Tente novamente.");
+        alertaSucesso.classList.remove("hidden");
+        alertaSucesso.classList.add("flex");
+        alertaSucesso.innerHTML = `
+          <div
+          role="alert"
+          class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+          >
+            <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+              d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              ></path>
+            </svg>
+            <p class="text-xs font-semibold">Erro - Erro interno do servidor.</p>
+          </div>
+        `
+        setTimeout(() => {
+          document.getElementById("alertaSucesso").classList.add("hidden");
+        }, 4000);
       });
   });
 });
