74615a198b83d8cdfc7b15688c63dcd3d645fdcf
MatAndrade01 <ltbmatheus141012@gmail.com>
2025-04-25 00:22:39 -0300

    Pop-ups da tela de criar, editar e remover variantes ta ok!

--- assets/scripts/criareditarVariavel.js
+++ assets/scripts/criareditarVariavel.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const token = localStorage.getItem("token");
   const searchInput = document.getElementById("searchInput");
+  const alertaSucesso = document.getElementById("alertaSucesso");
   let allProducts = [];
 
   fetch("https://back.soracoescariri.com.br/api/getProducts", {
@@ -12,6 +13,32 @@ document.addEventListener("DOMContentLoaded", function () {
   })
     .then((response) => {
       if (!response.ok) {
+        alertaSucesso.classList.remove("hidden");
+        alertaSucesso.innerHTML = `
+          <div
+          role="alert"
+          class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+          >
+            <svg
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            fill="none"
+            class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+              d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-width="2"
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              ></path>
+            </svg>
+            <p class="text-xs font-semibold">Erro - Erro interno do servidor.</p>
+          </div>
+        `
+        setTimeout(() => {
+          alertaSucesso.classList.add("hidden");
+        }, 4000);
         throw new Error(`Erro HTTP! Status: ${response.status}`);
       }
       return response.json();
@@ -20,8 +47,35 @@ document.addEventListener("DOMContentLoaded", function () {
       allProducts = data;
       renderProducts(allProducts);
     })
-    .catch((error) => console.error("Erro ao buscar os produtos:", error));
-
+    .catch((error) => {
+      alertaSucesso.classList.remove("hidden");
+      alertaSucesso.innerHTML = `
+        <div
+        role="alert"
+        class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+        >
+          <svg
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+            d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            stroke-width="2"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            ></path>
+          </svg>
+          <p class="text-xs font-semibold">Erro - Erro ao buscar produto.</p>
+        </div>
+      `
+      setTimeout(() => {
+        alertaSucesso.classList.add("hidden");
+      }, 4000);
+      console.error("Erro ao buscar os produtos:", error)
+    })
   function renderProducts(products) {
     const container = document.getElementById("product-container");
     if (!container) {
@@ -121,35 +175,170 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelectorAll(".remove-variante-btn").forEach((button) => {
       button.addEventListener("click", function () {
+        const body = document.querySelector("body");
+        const divBackground = document.getElementById("background");
+        const buttonSim = document.getElementById("buttonSim");
+        const buttonNao = document.getElementById("buttonNao");
         const productId = this.getAttribute("data-product-id");
         const selectedVariante = document.querySelector(`input[name="radio-${productId}"]:checked`);
 
         if (!selectedVariante) {
-          alert("Por favor, selecione uma variante para remover.");
+          alertaSucesso.classList.remove('hidden');
+          alertaSucesso.innerHTML = `
+            <div
+              role="alert"
+              class="bg-yellow-100 dark:bg-yellow-900 border-l-4 border-yellow-500 dark:border-yellow-700 text-yellow-900 dark:text-yellow-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-yellow-200 dark:hover:bg-yellow-800 transform hover:scale-105"
+            >
+              <svg
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                fill="none"
+                class="h-5 w-5 flex-shrink-0 mr-2 text-yellow-600"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+              <p class="text-xs font-semibold">
+                Alerta - Selecione uma variante.
+              </p>
+            </div>
+          `
+          setTimeout(() => {
+            alertaSucesso.innerHTML = '';
+            alertaSucesso.classList.add('hidden');
+          }, 4000);
           return;
         }
 
-        const varianteId = selectedVariante.value;
-        const confirmacao = confirm("Tem certeza que deseja remover esta variante?");
-        if (!confirmacao) return;
+        divBackground.classList.remove("hidden");
+        divBackground.classList.add("flex");
+        body.classList.add("overflow-hidden");
 
-        fetch(`https://back.soracoescariri.com.br/api/user/deleteVariantes/${varianteId}`, {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Erro ao remover variante");
-            }
-            return response.json();
+        const varianteId = selectedVariante.value;
+
+        const confirmar = (e) => {
+          e.preventDefault();
+
+          fetch(`https://back.soracoescariri.com.br/api/user/deleteVariantes/${varianteId}`, {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
           })
-          .then(() => {
-            selectedVariante.closest(".flex").remove();
-          })
-          .catch((error) => console.error("Erro ao remover variante:", error));
+            .then((response) => {
+              if (!response.ok) {
+                alertaSucesso.classList.remove("hidden");
+                alertaSucesso.innerHTML = `
+                  <div
+                  role="alert"
+                  class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+                  >
+                    <svg
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                      d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                      ></path>
+                    </svg>
+                    <p class="text-xs font-semibold">Erro - Erro ao remover variante.</p>
+                  </div>
+                `
+                setTimeout(() => {
+                  alertaSucesso.classList.add("hidden");
+                }, 4000);
+                throw new Error("Erro ao remover variante");
+              }
+              return response.json();
+            })
+            .then(() => {
+              selectedVariante.closest(".flex").remove();
+              fecharModal();
+              alertaSucesso.classList.remove("hidden");
+              alertaSucesso.innerHTML = `
+                <div
+                  role="alert"
+                  class="bg-green-100 dark:bg-green-900 border-l-4 border-green-500 dark:border-green-700 text-green-900 dark:text-green-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-green-200 dark:hover:bg-green-800 transform hover:scale-105"
+                >
+                  <svg
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    class="h-5 w-5 flex-shrink-0 mr-2 text-green-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      stroke-width="2"
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                    ></path>
+                  </svg>
+                  <p class="text-xs font-semibold">Sucesso - Produto excluido com sucesso!</p>
+                </div>
+              `
+              setTimeout(() => {
+                alertaSucesso.classList.add("hidden");
+              }, 7000);
+            })
+            .catch((error) => {
+              alertaSucesso.classList.remove("hidden");
+              alertaSucesso.innerHTML = `
+                <div
+                role="alert"
+                class="bg-red-100 dark:bg-red-900 border-l-4 border-red-500 dark:border-red-700 text-red-900 dark:text-red-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-red-200 dark:hover:bg-red-800 transform hover:scale-105"
+                >
+                  <svg
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  class="h-5 w-5 flex-shrink-0 mr-2 text-red-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                    d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    stroke-width="2"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    ></path>
+                  </svg>
+                  <p class="text-xs font-semibold">Erro - Erro Interno do servidor.</p>
+                </div>
+              `
+              setTimeout(() => {
+                alertaSucesso.classList.add("hidden");
+              }, 4000);
+              console.error("Erro ao remover variante:", error);
+            });
+        };
+
+        const cancelar = (e) => {
+          e.preventDefault();
+          fecharModal();
+        };
+
+        const fecharModal = () => {
+          divBackground.classList.remove("flex");
+          divBackground.classList.add("hidden");
+          body.classList.remove("overflow-hidden");
+          buttonSim.removeEventListener("click", confirmar);
+          buttonNao.removeEventListener("click", cancelar);
+        };
+
+        buttonSim.addEventListener("click", confirmar);
+        buttonNao.addEventListener("click", cancelar);
       });
     });
   }
